138b11ec7688f405f859d426305a6dc71220cd41
Denys Domanskyi <denys@tegro.com>
2023-08-29 17:17:02 +0300

    trying partial endpoint for markets from strapi

--- pages/market/[[...segments]].js
+++ pages/market/[[...segments]].js
@@ -52,17 +52,19 @@ export default function Markets({marketData, marketSales, marketOrders}) {
   }, [marketData, marketSales, marketOrders])
 
   useEffect(() => {
-    fetch('https://strapi.tegro.com/api/markets', {method: 'GET', headers: { Authorization: `Bearer ${token}` }})
+    const id = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    fetch('https://strapi.tegro.com/api/markets/?filters[contract_address][$eq]='+id, {method: 'GET', headers: { Authorization: `Bearer ${token}` }})
+    // fetch('https://strapi.tegro.com/api/markets', {method: 'GET', headers: { Authorization: `Bearer ${token}` }})
       .then((res) => res.json())
       .then(({data}) => {
         console.log('data', data);
-        const [currentMarket] = data.filter(item => {
-          return item.attributes.token_metadata[0].contract_address === queryMarketId
-        })
+        // const [currentMarket] = data.filter(item => {
+        //   return item.attributes.token_metadata[0].contract_address === queryMarketId
+        // })
 
-        if (currentMarket.attributes) {
-          dispatch($collection.set.currentMarketSeoInfo(currentMarket.attributes))
-        }
+        // if (currentMarket.attributes) {
+        //   dispatch($collection.set.currentMarketSeoInfo(currentMarket.attributes))
+        // }
         // console.log('currentMarket', currentMarket.attributes);
       })
   }, [])
