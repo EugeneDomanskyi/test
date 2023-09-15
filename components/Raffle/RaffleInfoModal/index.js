e947f2b29817224efacab578aa52685d66dfd661
Denys Domanskyi <denys@tegro.com>
2023-09-15 12:33:31 +0300

    implementing RaffleRoulette

---
+++ components/Raffle/RaffleInfoModal/index.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux'
+import Image from 'next/image'
+
+import $app from '@/store/app'
+
+import App from '@/components/App'
+import Raffle from '@/components/Raffle'
+
+import styles from './styles.module.scss'
+
+const RaffleInfoModal = ({}) => {
+  const blockchain = useSelector($app.get.blockchain)
+
+  return (
+    <App.Flex column gap={32} align="center" className={styles.content}>
+      <App.Flex column gap={4}>
+        <Raffle.Roulette />
+      </App.Flex>
+    </App.Flex>
+  )
+}
+
+export default RaffleInfoModal
