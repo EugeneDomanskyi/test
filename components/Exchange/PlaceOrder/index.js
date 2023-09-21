2db9afc58e28f2d3c8a2a7017afc1f85777a4f9a
JulianBeletskyy <julian@tegro.com>
2023-09-21 14:06:29 +0300

    fix string

--- components/Exchange/PlaceOrder/index.js
+++ components/Exchange/PlaceOrder/index.js
@@ -158,7 +158,7 @@ const PlaceOrder = ({data, onClose}) => {
                 <App.Flex column align="center" sx={{paddingTop: 40, paddingBottom: 40}} gap={16} className={styles.content}>
                   <App.Icon icon="check-circle-fill" />
                   <App.Text size={20} weight={700} center>Order Created</App.Text>
-                  <App.Text size={14} weight={500} center>Your order has been successfully placed in the orderbook! You can track its progress in the "My Orders" section</App.Text>
+                  <App.Text size={14} weight={500} center>{`Your order has been successfully placed in the orderbook! You can track its progress in the "My Orders" section`}</App.Text>
                   <App.Flex column className={styles.border} sx={{width: '100%'}}>
                     <App.Flex justify="space-between" direction={data.side === 'buy' ? 'row' : 'row-reverse'}>
                       <App.Flex align="center">
