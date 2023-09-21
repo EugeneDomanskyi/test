c05ccd11dc28bd367c6ee8d77b83c7e0e55e4680
JulianBeletskyy <julian@tegro.com>
2023-09-21 13:24:56 +0300

    Update result order place screen

--- components/Exchange/PlaceOrder/index.js
+++ components/Exchange/PlaceOrder/index.js
@@ -157,8 +157,8 @@ const PlaceOrder = ({data, onClose}) => {
               return (
                 <App.Flex column align="center" sx={{paddingTop: 40, paddingBottom: 40}} gap={16} className={styles.content}>
                   <App.Icon icon="check-circle-fill" />
-                  <App.Text size={20} weight={700} center>Order In Progress</App.Text>
-                  <App.Text size={14} weight={500} center>We will inform you once the order is filled completely. Meanwhile you can keep track through the ongoing order list</App.Text>
+                  <App.Text size={20} weight={700} center>Order Created</App.Text>
+                  <App.Text size={14} weight={500} center>Your order has been successfully placed in the orderbook! You can track its progress in the "My Orders" section</App.Text>
                   <App.Flex column className={styles.border} sx={{width: '100%'}}>
                     <App.Flex justify="space-between" direction={data.side === 'buy' ? 'row' : 'row-reverse'}>
                       <App.Flex align="center">
