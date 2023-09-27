6b06a910b7bc9e00f0c7ad2d1bd40fe031219937
Denys Domanskyi <denys@tegro.com>
2023-09-27 14:36:27 +0300

    changed default network to Mumbai for testing

--- pages/raffle/[[...segments]].js
+++ pages/raffle/[[...segments]].js
@@ -128,7 +128,8 @@ const RafflePage = () => {
       }))))
     }
 
-    const networkCode = process.env.NEXT_PUBLIC_APP_ENV == 'local' ? 'mumbai' : 'polygon'
+    // const networkCode = process.env.NEXT_PUBLIC_APP_ENV == 'local' ? 'mumbai' : 'polygon'
+    const networkCode = 'mumbai'
     const network = await changeNetwork(networkCode)
     if ( ! network) {
       return
