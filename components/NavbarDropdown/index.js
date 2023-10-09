dcf3aa1be4b9284e17ef72a25f6c777a5680d884
Denys Domanskyi <denys@tegro.com>
2023-10-09 19:46:09 +0300

    finished redesign new header

--- components/NavbarDropdown/index.js
+++ components/NavbarDropdown/index.js
@@ -18,150 +18,144 @@ import SwitchBlockchain from '@/components/SwitchBlockchain'
 import styles from './styles.module.scss'
 
 const NavbarDropdown = ({isOpen, onClose}) => {
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, false)
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside, false)
-    }
-  }, [])
-
-  const handleClickOutside = (event) => {
-    if (! event.target.closest('#menu-dropdown')) {
-      console.log('click outside', event.target.closest('#menu-dropdown'));
-      onClose()
-    }
+  const handleClickOutside = () => {
+    console.log('handleClickOutside');
+    onClose()
   }
 
   return (
-    <App.Flex column gap={32} className={cn(styles.dropdownMenu, {[styles.isOpen]: isOpen})}>
-      <App.Flex gap={24}>
-        <App.Flex column gap={24}>
-          <App.Text size={14} weight={700} color="#B9B8C5">EXCHANGE</App.Text>
+    <>
+      <App.Flex sx={{position: 'fixed', inset: 0, width: '100vw', height: '100vh', zIndex: 10, display: isOpen ? 'flex' : 'none', cursor: 'defaul'}} onClick={handleClickOutside} />
 
-          <App.Flex column gap={8}>
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuNFT" />
+      <App.Flex column gap={32} className={cn(styles.dropdownMenu, {[styles.isOpen]: isOpen})}>
+        <App.Flex gap={24}>
+          <App.Flex column gap={24}>
+            <App.Text size={14} weight={700} color="#B9B8C5">EXCHANGE</App.Text>
+
+            <App.Flex column gap={8}>
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuNFT" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>NFT {`->`}</App.Text>
+                  <App.Text size={10} weight={500}>Trade NFTs as easy as ERC-20 Tokens</App.Text>
+                </App.Flex>
               </App.Flex>
+              
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuSWAP" />
+                </App.Flex>
 
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>NFT {`->`}</App.Text>
-                <App.Text size={10} weight={500}>Trade NFTs as easy as ERC-20 Tokens</App.Text>
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>NFT Swap</App.Text>
+                  <App.Text size={10} weight={500}>Buy & Sell NFTs in bulk at the best prices</App.Text>
+                </App.Flex>
+              </App.Flex>
+              
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuClassic" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>Classic Tegro Withdraw</App.Text>
+                  <App.Text size={10} weight={500}>Classic Tegro user? Withdraw your funds now</App.Text>
+                </App.Flex>
               </App.Flex>
             </App.Flex>
-            
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuSWAP" />
-              </App.Flex>
+          </App.Flex>
 
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>NFT Swap</App.Text>
-                <App.Text size={10} weight={500}>Buy & Sell NFTs in bulk at the best prices</App.Text>
-              </App.Flex>
-            </App.Flex>
-            
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuClassic" />
-              </App.Flex>
+          <App.Flex column gap={24}>
+            <App.Text size={14} weight={700} color="#B9B8C5">RESOURCES</App.Text>
 
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>Classic Tegro Withdraw</App.Text>
-                <App.Text size={10} weight={500}>Classic Tegro user? Withdraw your funds now</App.Text>
+            <App.Flex column gap={8}>
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuBlog" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>Blog</App.Text>
+                  <App.Text size={10} weight={500}>Discover insights, stories, and updates</App.Text>
+                </App.Flex>
+              </App.Flex>
+              
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuGitbook" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>Gitbook</App.Text>
+                  <App.Text size={10} weight={500}>Read product documentation and guides</App.Text>
+                </App.Flex>
+              </App.Flex>
+              
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuPress" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>Press</App.Text>
+                  <App.Text size={10} weight={500}>Explore latest news and media mentions</App.Text>
+                </App.Flex>
+              </App.Flex>
+              
+              <App.Flex gap={8} className={styles.menuItem}>
+                <App.Flex>
+                  <App.Icon icon="menuContact" />
+                </App.Flex>
+
+                <App.Flex column sx={{width: 160}}>
+                  <App.Text size={14} weight={500}>Contact</App.Text>
+                  <App.Text size={10} weight={500}>Reach out for inquiries or collaborations</App.Text>
+                </App.Flex>
               </App.Flex>
             </App.Flex>
           </App.Flex>
         </App.Flex>
-
-        <App.Flex column gap={24}>
-          <App.Text size={14} weight={700} color="#B9B8C5">RESOURCES</App.Text>
-
-          <App.Flex column gap={8}>
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuBlog" />
-              </App.Flex>
-
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>Blog</App.Text>
-                <App.Text size={10} weight={500}>Discover insights, stories, and updates</App.Text>
-              </App.Flex>
-            </App.Flex>
-            
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuGitbook" />
-              </App.Flex>
-
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>Gitbook</App.Text>
-                <App.Text size={10} weight={500}>Read product documentation and guides</App.Text>
-              </App.Flex>
-            </App.Flex>
-            
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuPress" />
-              </App.Flex>
-
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>Press</App.Text>
-                <App.Text size={10} weight={500}>Explore latest news and media mentions</App.Text>
-              </App.Flex>
-            </App.Flex>
-            
-            <App.Flex gap={8} className={styles.menuItem}>
-              <App.Flex>
-                <App.Icon icon="menuContact" />
-              </App.Flex>
-
-              <App.Flex column sx={{width: 160}}>
-                <App.Text size={14} weight={500}>Contact</App.Text>
-                <App.Text size={10} weight={500}>Reach out for inquiries or collaborations</App.Text>
-              </App.Flex>
-            </App.Flex>
-          </App.Flex>
-        </App.Flex>
-      </App.Flex>
-      
-      <App.Flex column gap={16}>
-        <App.Text>JOIN OUR COMMUNITY</App.Text>
         
-        <App.Flex gap={16}>
-          <App.Flex center gap={4}>
-            <App.Icon icon="twitter-filled" />
-            <App.Text size={10} weight={500}>Twitter</App.Text>
-          </App.Flex>
+        <App.Flex column gap={16}>
+          <App.Text>JOIN OUR COMMUNITY</App.Text>
           
-          <App.Flex center gap={4}>
-            <App.Icon icon="discord-filled" />
-            <App.Text size={10} weight={500}>Discord</App.Text>
-          </App.Flex>
-          
-          <App.Flex center gap={4}>
-            <App.Icon icon="telegram-filled" />
-            <App.Text size={10} weight={500}>Telegram</App.Text>
-          </App.Flex>
-          
-          <App.Flex center gap={4}>
-            <App.Icon icon="linkedin-filled" />
-            <App.Text size={10} weight={500}>Linkdein</App.Text>
-          </App.Flex>
-          
-          <App.Flex center gap={4}>
-            <App.Icon icon="substack-filled" />
-            <App.Text size={10} weight={500}>Substack</App.Text>
-          </App.Flex>
-          
-          <App.Flex center gap={4}>
-            <App.Icon icon="youtube-filled" />
-            <App.Text size={10} weight={500}>Youtube</App.Text>
+          <App.Flex gap={16}>
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="twitter-filled" />
+              <App.Text size={10} weight={500}>Twitter</App.Text>
+            </App.Flex>
+            
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="discord-filled" />
+              <App.Text size={10} weight={500}>Discord</App.Text>
+            </App.Flex>
+            
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="telegram-filled" />
+              <App.Text size={10} weight={500}>Telegram</App.Text>
+            </App.Flex>
+            
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="linkedin-filled" />
+              <App.Text size={10} weight={500}>Linkdein</App.Text>
+            </App.Flex>
+            
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="substack-filled" />
+              <App.Text size={10} weight={500}>Substack</App.Text>
+            </App.Flex>
+            
+            <App.Flex center gap={4} className={styles.socialLink}>
+              <App.Icon icon="youtube-filled" />
+              <App.Text size={10} weight={500}>Youtube</App.Text>
+            </App.Flex>
           </App.Flex>
         </App.Flex>
       </App.Flex>
-    </App.Flex>
+    </>
   )
 }
 
