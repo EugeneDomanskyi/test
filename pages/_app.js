558f6ea052b52f3c7921ff66a032f6d8123bb2c2
JulianBeletskyy <julian@tegro.com>
2023-10-12 11:48:08 +0300

    increase performance across switching tokens

--- pages/_app.js
+++ pages/_app.js
@@ -47,6 +47,8 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+let firstTimeLoaded = false
+
 createClient({
   chains: CHAINS,
   source: "tegro.com"
@@ -198,11 +200,12 @@ MyApp.getInitialProps = async ({ctx}) => {
     const [addrArr] = routeArr.slice(-1)
     currentAddress = addrArr.split('?')[0]
     ssRoute = (ctx.req.url)
-    if (ctx.req.url.includes('market') || ctx.req.url.includes('tokens')) {
+    if (!firstTimeLoaded) {
       const list = await getAssetsFile()
       if (list && Array.isArray(list)) {
         marketsList = list
         marketInfo = list.find(item => item.address === currentAddress) || {}
+        firstTimeLoaded = true
       }
     }
   }
