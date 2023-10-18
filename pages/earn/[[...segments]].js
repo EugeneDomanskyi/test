c79c1129302ae553472f9f597b63ef8bc3a61b4e
Denys Domanskyi <denys@tegro.com>
2023-10-18 16:03:20 +0300

    fixed meta tags image

--- pages/earn/[[...segments]].js
+++ pages/earn/[[...segments]].js
@@ -299,8 +299,8 @@ const RafflePage = () => {
       <Head>
         <title>$10,000+ Tegro Treasure Case Series Live Now! | Tegro Earn</title>
         <meta content="Join the $10,000+ Tegro Treasure Case Series today! Complete trading objectives to obtain TKeys and unlock cases to win exciting $USDT, $PEPE, $SHIB, $FLOKI, and other token rewards! Enter now." property="description" key="description" />
-        <meta property="og:image" content="summary_large_image" />
-        <meta name="twitter:card" content="https://tegro-imagekit-tora.s3.eu-central-1.amazonaws.com/images/tegro-earn.jpg" />
+        <meta property="og:image" content="https://tegro-imagekit-tora.s3.eu-central-1.amazonaws.com/images/tegro-earn.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@TegroFi" />
         <meta name="twitter:title" content="$10,000+ Tegro Treasure Case Series Live Now! | Tegro Earn" />
         <meta name="twitter:description" content="Join the $10,000+ Tegro Treasure Case Series today! Complete trading objectives to obtain TKeys and unlock cases to win exciting $USDT, $PEPE, $SHIB, $FLOKI, and other token rewards! Enter now." />
