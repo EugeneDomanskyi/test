51acb0e0902a12e2a0dee7ceee47420b01eff31e
Denys Domanskyi <denys@tegro.com>
2023-11-16 13:55:55 +0200

    fixed issue with application error on mobile

--- components/Wrapper/WrapperExchange/index.js
+++ components/Wrapper/WrapperExchange/index.js
@@ -27,7 +27,7 @@ const WrapperExchange = ({children, _isMobile}) => {
   const [wrongAddress, setWrongAddress] = useState(false)
 
   const [urlAddress] = router.query.address || []
-  const address = urlAddress.toLowerCase()
+  const address = urlAddress ? urlAddress?.toLowerCase() : ''
   const blockchain = router.query.blockchain
   const isAddress = /^(0x)?[0-9a-fA-F]{40}$/.test(address)
   const emptyAddress = address === '0x'
