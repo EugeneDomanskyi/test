0633277a397fb0fe58d83d509dc728da7736e7a4
Eugene Domanskyi <eugene@tegro.com>
2023-12-08 14:23:57 +0200

    Fix orders update

--- components/Exchange/Orders/index.js
+++ components/Exchange/Orders/index.js
@@ -18,6 +18,7 @@ import useWalletConnect from '@/myhooks/wallet-connect'
 import useOrders from '@/myhooks/useOrders'
 
 import OrderDetails from '@/components/Exchange/OrderDetails'
+import useInterval from '@/myhooks/useInterval'
 
 const Orders = ({global, type, version, onClickOrder}) => {
   const router = useRouter()
@@ -259,7 +260,7 @@ const Orders = ({global, type, version, onClickOrder}) => {
     return type !== 'tokens' || !hideCancelledOrders || (order.status !== 'cancelled')
   }
 
-  // useInterval(getOrders, wallet ? 15000 : null)
+  useInterval(getOrders, wallet ? 5000 : null)
 
   return (
     <App.Flex column className={cn(styles.container, {[styles[version]]: version})}>
