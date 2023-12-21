5105c1340281748017ebe4bb5a7863754b2c5962
Denys Domanskyi <denys@tegro.com>
2023-12-21 11:52:21 +0200

    added headers to endpoint calls

--- libs/magic-square.lib.js
+++ libs/magic-square.lib.js
@@ -1,12 +1,16 @@
 export const tradeVolumeCheck = async (address) => {
   const vid = localStorage.getItem('ms_vid')
+  const headers = {
+    "Content-Type": "application/json",
+  }
+
   if (vid) {
     try {
       const post = {
         wallet_address: address,
         vid: vid
       }
-      fetch(`https://us-central1-vibrant-waters-399406.cloudfunctions.net/magic_square_trade_volume_check`, { method: 'POST', body: JSON.stringify(post) })
+      fetch(`https://us-central1-vibrant-waters-399406.cloudfunctions.net/magic_square_trade_volume_check`, { method: 'POST', body: JSON.stringify(post), headers })
     } catch (err) {
       console.log('err', err);
     }
@@ -15,11 +19,14 @@ export const tradeVolumeCheck = async (address) => {
 
 export const connectWalletVid = async address => {
   const vid = localStorage.getItem('ms_vid')
+  const headers = {
+    "Content-Type": "application/json",
+  }
   if (vid && address) {
     const post = {
       wallet_address: address,
       vid: vid
     }
-    fetch(`https://us-central1-vibrant-waters-399406.cloudfunctions.net/connect-wallet-vid`, { method: 'POST', body: JSON.stringify(post) })
+    fetch(`https://us-central1-vibrant-waters-399406.cloudfunctions.net/connect-wallet-vid`, { method: 'POST', body: JSON.stringify(post), headers})
   }
 }
