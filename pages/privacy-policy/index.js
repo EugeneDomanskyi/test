ea7b0dd48077e8305864477f429d7aa10c016c90
Denys Domanskyi <denys@tegro.com>
2024-01-31 11:17:02 +0200

    added Privacy Policy page

---
+++ pages/privacy-policy/index.js
@@ -0,0 +1,239 @@
+import App from '@/components/App'
+
+import styles from './styles.module.scss'
+
+export default function LandingPage() {
+  return (
+    <App.Container className={styles.container}>
+      <App.Flex column align="center">
+        <h1>PRIVACY POLICY</h1>
+
+        <p>This privacy policy (<b>“Privacy Policy”</b>) sets out how Tegro (<b>“Company”</b>) uses and protects any information that you give the Company when you use or access the <a href="http://tegro.com/">http://tegro.com/</a>, Tegro (“Application”), or any services or materials provided by the Company (collectively “Services”). The Company is committed to protecting your privacy. This Privacy Policy explains how the Company collects, uses, and shares your personal information. This Privacy Policy applies to your use of the Service as further described in the Company's Terms and Conditions available at <a href="https://Tegro.com/terms">https://tegro.com/terms</a>.</p>
+
+        <p>By using the Services, you agree to be bound by the terms and conditions of this Privacy Policy. You hereby expressly consent to the processing of your personal information in accordance with this Privacy Policy. Your personal information may be stored, processed, or transmitted in India or any other part of the world, where laws regarding storing, processing, or transmitting personal information may be less restrictive than the laws in your country. If you do not agree to the terms of this Privacy Policy, please do not use or access the Services. By mere use of or access to the Services, you expressly consent to the Company's use and disclosure of your personal information in accordance with this Privacy Policy. This Privacy Policy is incorporated into and subject to the Terms and Conditions.</p>
+
+        <h2>TYPES OF INFORMATION THE COMPANY COLLECTS</h2>
+
+        <p>The Company collects personal information from you in a variety of ways when you use Services, such as:</p>
+
+        <p>
+            <strong>Account.</strong> The Company collects, and associates with your User Account (as defined below) information, log-in credentials, password, your name, birth date, email address, phone number, profile details, gender, address, and other personally identifiable information which may include without limitation:
+            <ul>
+              <li>Data on physical characteristics such as weight, height or body measurement;</li>
+              <li>Photographs, videos, audios, text, graphics, or any other related content;</li>
+              <li>Payment or credit, debit card information;</li>
+              <li>Personal Medical information such as physical or mental health issues, illness, injury, or any other medical condition;</li>
+              <li>Lifestyle information such as nutrition data, workouts preference, physical activity, eating and sleep patterns, and other habits;</li>
+              <li>Information that you provide when you write directly to Company and/or Registered Trainers (as defined in our Terms and Conditions) (including by way of e-mail);</li>
+              <li>Information that you provide to the Company and/or Registered Trainers (as defined in our Terms and Conditions) over the telephone. The Company may make and keep a record of the information shared by you with the Company and/or Registered Trainers.</li>
+            </ul>
+
+            If you create an account (<b>“User Account”</b>) by logging in with your credentials from your third-party account with certain social networking sites (such as Google+ or Facebook), the Company may receive information about you from such social networking sites, in accordance with the terms of use and privacy policy of that social networking site. The Company works with such social networking site's application protocol interface (API) in a way that allows you to authorize the Company to access your account on that social networking site on your behalf. If you consent to the Company to share your information with these social networking sites (e.g., post public content on your wall/profile page), the Company will share information with them in accordance with your privacy choices. Once the Company shares your information with these social networking sites, the terms and policies of these sites, and not this Privacy Policy, will apply to their use of your information.
+        </p>
+        <p>
+          <strong>Website and Application Related Use.</strong> When you use the Services, the Company may store, process and
+          transmit your files containing personally identifiable details (like images, videos and audios) and information
+          related to them with you prior consent. However, the Company shall have a free right to store, use, process or
+          transmit any feedback, postings or comments that you may provide.
+        </p>
+        <p>
+          <strong>Usage.</strong> The Company collects information about your medical and physical data and preferences as per the
+          data input by you for the purpose of provisioning of the Services to you. Your devices (depending on their
+          settings) may also transmit location information to the Company.
+        </p>
+        <p>
+          <strong>Cookies and other Technologies.</strong> The Company uses technologies like cookies and pixel tags to provide,
+          improve, protect and promote the Services. For example, cookies help the Company with things like
+          remembering your username for your next visit, understanding how you are interacting with the Services, and
+          improving it based on that information. The Company uses software tools to collect information about how you
+          interact with the Services, such as mouse clicks, touch gestures, drags, hover overs, response times, errors, -
+          page views and length of visits to certain pages. You can set your browser to not accept cookies, but this may
+          limit your ability to use the Services. The Company may ask its other partners to serve ads or services to your
+          computer systems or other devices, which may use cookies or similar technologies placed by the Company or
+          the third party. The Company does not control use of such technologies used by advertisers and partners and it
+          expressly disclaims responsibility for information collected through them.
+        </p>
+        <p>
+          <strong>Analytic Services.</strong> The Company may use third party analytics services such as Google Analytics, Hotjar,
+          Microsoft Clarity, KISSmetrics and Comscore, to collect information about how you use and interact with the
+          Services. Such third party analytics services may use cookies to gather information such as the pages you
+          visited, your IP address, a date/time stamp for your visit and which website or hyperlink referred you to the
+          Services. The Company uses these analytics services to analyze how people use the Services, to improve them,
+          to customize the content/features users see/use based upon their interests and to send email to users who are
+          not actively using the Services. For more information about how these entities use this information, please
+          refer to their privacy policies made available on their respective websites.
+        </p>
+        <p>
+          <strong>Information Collected from Third Parties.</strong> The Company may receive information about you from third parties
+          that offer hyperlinks to the Services on their website and/or applications and/or products and/or services.
+          These third parties may supply the Company with your information. The Company may add this information to
+          the information it has already collected from you via the Services in order to improve the products and/or
+          services the Company provides and/or its website and software.
+        </p>
+
+        <h2>HOW THE COMPANY USES YOUR INFORMATION</h2>
+
+        <p>
+          <strong>General Use.</strong> In general, the information you submit to the Company is used either to respond to requests that
+          you make, or to aid the Company in serving you better. The Company may use your information in the
+          following ways:
+
+          <ul>
+            <li>to send you a welcome e mail - and to verify ownership of the e mail - address provided when your User Account is created;</li>
+            <li>to identify you as a user in the Company's system;</li>
+            <li>to provide access to the Services;</li>
+            <li>to facilitate the creation of and secure your User Account;</li>
+            <li>to provide improved Services;</li>
+            <li>to notify you about updates to the Services;</li>
+            <li>to provide you transaction summary on making applicable payments for the Services, if any;</li>
+            <li>to improve and customize the quality of experience when you use the Services;</li>
+            <li>to send you administrative e mail - notifications, such as security or support and maintenance advices;</li>
+            <li>to engage with or contact inactive users of the Services;</li>
+            <li>to direct certain content and advertisements to you so that you are more likely to see content and advertisements that are relevant to you;</li>
+            <li>to analyze the data submitted by you; or</li>
+            <li>to send offers and promotional materials related to the Services and those of third parties for marketing and other purposes.</li>
+          </ul>
+        </p>
+
+        <p>
+          The Company may also use and share aggregated or de identified information for any purpose and in any -
+          manner. This anonymous data that the Company shares may include non personally identifiable data that the -
+          Company creates using your personal information by excluding information that makes the data personally
+          identifiable.
+        </p>
+
+        <h2>HOW THE COMPANY SHARES YOUR INFORMATION</h2>
+
+        <p>
+          The Company may share information as per the terms of this Privacy Policy, however the Company shall
+          endeavor that such use of information does not create hindrance on your use of the Services. The Company
+          does not share your personal information with third parties other than as follows:
+        </p>
+
+        <p>
+          <strong>Third Parties.</strong> The Company may also share certain information with third party advertising partners. This -
+          information would allow third party ad networks to, among other things, deliver targeted advertisements that -
+          they believe will be of most interest to you. The Company may also share certain information with its affiliates,
+          Registered Trainers (as defined in our Terms and Conditions), partners and or third party platforms that provide
+          any services and/or products on or through the Platform (as defined in our Terms and Conditions).
+        </p>
+
+        <p>
+          <strong>Generally with your Consent.</strong> Any information or content that you voluntarily disclose for posting on the
+          Services may become available to the public, as controlled by any applicable privacy settings that you set. To
+          change your privacy settings on the Services, you may change your profile setting. Once you have shared the
+          content/data or made it public, that content/data may be re shared - by others.
+        </p>
+
+        <p>
+          <strong>Enforcing Rights.</strong> The Company may also disclose personal information to enforce its policies, respond to
+          claims that a posting or other content violates others' rights, or protects anyone's rights, property or safety.
+        </p>
+
+        <p>
+          <strong>Law & Order.</strong> The Company may disclose your information to third parties if it determines that such disclosure
+          is reasonably necessary to: (a) comply with the laws; (b) protect any person from death or serious bodily injury;
+          (c) prevent fraud or abuse of its name, brands, trademarks or such other rights belonging to it or its other
+          users; or (d) protect its rights.
+        </p>
+
+        <p>
+          Stewardship of your data is critical to the Company and a responsibility that the Company embraces. The
+          Company believes that its users' data should receive the same legal protections regardless of whether it's
+          stored on its servers or on your home computer systems or devices. The Company employs and protects all
+          data with data encryption, anti malware protection and other methods as per the industry standards. The -
+          Company will abide by the following principles when receiving, scrutinizing and responding to government
+          requests for its users' data:
+
+          <ul>
+            <li>Be transparent,</li>
+            <li>Fight blanket requests,</li>
+            <li>Protect all users, and</li>
+            <li>Provide trusted services.</li>
+          </ul>
+        </p>
+
+        <h2>STORING OF INFORMATION</h2>
+
+        <p>
+          <strong>Retention.</strong> The Company will retain your personal information for a reasonable period or period mandated by
+          statute, whichever is longer.
+        </p>
+
+        <p>
+          <strong>No warranty.</strong> The Company uses commercially reasonable safeguards to help keep the information collected
+          through the Services secure and take reasonable steps (such as requesting a unique password) to verify your
+          identity before granting you access to your User Account. However, no method of transmission over the
+          internet, or method of electronic storage, is 100% secure and so the Company cannot fully ensure or guarantee
+          the security of any information you transmit to the Company or guarantee that information on the Services
+          may not be accessed, disclosed, altered, or destroyed by any person.
+        </p>
+
+        <p>
+          <strong>Around the world.</strong> The Company may store, process and transmit information in locations around the world -
+          including those outside your country. Information may also be stored locally on the devices you use to access
+          the Services. By registering for and using the Services, you consent to the transfer of information to any part of
+          the world in which the Company, its affiliates or service providers maintain facilities and the use and disclosure
+          of information about you thereto as described in this Privacy Policy.
+        </p>
+
+        <h2>CHANGES TO YOUR INFORMATION</h2>
+
+        <p>
+          You may change certain of your personal information by editing your User Account settings or by sending the
+          Company an e mail - at <a href="mailto:compliance@tegro.com">compliance@tegro.com</a> or postal mail addressed to the Company. The Company
+          may maintain a copy of your unrevised information in its records, even after your revise or update your
+          information. Some of your information may remain in the Company's private records after your deletion of
+          such information from your User Account. You are responsible for maintaining the secrecy of your unique
+          password and account information, limiting access to your computer or device and browser by signing off after
+          you have finished accessing your User Account and for controlling access to emails between you and the
+          Company, at all times. The Company is not responsible for any third party functionality, privacy or security
+          policies which you are bound by. Please do your part to help the Company.
+        </p>
+
+        <h2>YOUR RIGHTS</h2>
+
+        <p>
+          You have the right to request the Company: (I) to provide access to your personal information; (II) for
+          correction of your personal information, if it is inaccurate; or (III) for deletion of your personal information. You
+          also have the right to object to the processing of your personal information on compelling and genuine
+          purpose, including for marketing purposes. In place where the Company has obtained your consent for
+          processing your personal information, you have the right to withdraw your consent at any time.
+        </p>
+
+        <h2>CHANGES</h2>
+
+        <p>
+          The Company may share some or all of your information in connection with or during negotiation of any
+          merger, financing, acquisition or dissolution, collaboration, transaction or proceeding involving sale, transfer,
+          divestiture, or disclosure of all or a portion of its business or assets. In the event of an insolvency, bankruptcy,
+          or receivership, your information may also be transferred as a business asset. If another entity acquires the
+          Company or its business or assets, that entity will possess all your information collected by the Company and
+          will assume the rights and obligations regarding your information as described in this Privacy Policy.
+        </p>
+
+        <p>
+          The Company may change this Privacy Policy from time to time. If the Company makes any changes to this
+          Privacy Policy, the last modified Privacy Policy shall be applicable to you.
+        </p>
+
+        <h2>COMMUNICATIONS</h2>
+
+        <p>
+          The Company may periodically send you free newsletters and promotional communications. When you receive
+          newsletters or promotional communications from the Company, you may “opt out” - of these emails by
+          following the unsubscribe instructions provided in the email you receive or by contacting the Company at <a href="mailto:compliance@tegro.com">compliance@tegro.com</a>.
+          Despite your indicated email preferences, the Company may send you notices of any
+          updates to its Privacy Policy, Terms and Conditions or other administrative emails.
+        </p>
+
+        <h2>CONTACT</h2>
+
+        <p>
+          The Company welcomes your comments, grievances or questions regarding this Privacy Policy. Please e-mail
+          the Company at <a href="mailto:compliance@tegro.com">compliance@tegro.com</a>
+        </p>
+      </App.Flex>
+    </App.Container>
+  )
+}
