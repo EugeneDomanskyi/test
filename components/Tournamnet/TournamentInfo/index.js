ebc461be3c845e1e8a0a64bc23fe80481cff2b16
EugeneDomanskyi <eugene@tegro.com>
2024-03-05 10:36:44 +0200

    Fix with connect Wallet

--- components/Tournamnet/TournamentInfo/index.js
+++ components/Tournamnet/TournamentInfo/index.js
@@ -14,7 +14,7 @@ import styles from './styles.module.scss'
 
 const TournamentInfo = ({ tournament }) => {
   const router = useRouter()
-  const { wallet } = useWalletConnect()
+  const { wallet, connect, connection } = useWalletConnect()
 
   const isMobile = useSelector(({ $app }) => $app.size.isMobile)
 
@@ -80,105 +80,115 @@ const TournamentInfo = ({ tournament }) => {
     setOpenModal(true)
   }
 
-  return (
-    <App.Flex column fullWidth gap={24}>
-      <App.Flex row fullWidth gap={24}>
-        <App.Frame flex={1} padding={0} radius={16} background="radial-gradient(134.18% 120.73% at 67.99% -35.17%, #231F38 0%, #100D21 100%)" gradient="linear-gradient(101.49deg, #749828 -1.14%, #674EFF 109.57%)">
-          <App.Flex direction={['row', 'column']} sx={[{ padding: 12 }, { padding: '8px 12px' }]} align={['center', 'flex-start']} justify="space-between" height={[68, 'auto']}>
-            <App.Text size={[16, 14]} weight={700}>Current <App.Text inline italic family="Playfair Display" size={[16, 14]} weight={700}>Position</App.Text></App.Text>
+  const handleConnect = () => {
+    connect()
+  }
+
+  return ! connection.loading ? (
+    connection.connected ? (
+      <App.Flex column fullWidth gap={24}>
+        <App.Flex row fullWidth gap={24}>
+          <App.Frame flex={1} padding={0} radius={16} background="radial-gradient(134.18% 120.73% at 67.99% -35.17%, #231F38 0%, #100D21 100%)" gradient="linear-gradient(101.49deg, #749828 -1.14%, #674EFF 109.57%)">
+            <App.Flex direction={['row', 'column']} sx={[{ padding: 12 }, { padding: '8px 12px' }]} align={['center', 'flex-start']} justify="space-between" height={[68, 'auto']}>
+              <App.Text size={[16, 14]} weight={700}>Current <App.Text inline italic family="Playfair Display" size={[16, 14]} weight={700}>Position</App.Text></App.Text>
+              {loading ? (
+                <App.Loader size={[40, 32]} />
+              ) : (
+                <App.Text size={[40, 32]} weight={700}>{results.position}</App.Text>
+              )}
+            </App.Flex>
+          </App.Frame>
+
+          <App.Flex direction={['row', 'column']} flex={1} className={styles.pointsBox} align={['center', 'flex-start']} justify="space-between" height={[68, 'auto']}>
+            <App.Text size={[16, 14]} weight={700}>Points <App.Text inline italic family="Playfair Display" size={[16, 14]} weight={700}>Earned</App.Text></App.Text>
             {loading ? (
-              <App.Loader size={[40, 32]} />
-            ) : (
-              <App.Text size={[40, 32]} weight={700}>{results.position}</App.Text>
-            )}
-          </App.Flex>
-        </App.Frame>
-
-        <App.Flex direction={['row', 'column']} flex={1} className={styles.pointsBox} align={['center', 'flex-start']} justify="space-between" height={[68, 'auto']}>
-          <App.Text size={[16, 14]} weight={700}>Points <App.Text inline italic family="Playfair Display" size={[16, 14]} weight={700}>Earned</App.Text></App.Text>
-          {loading ? (
-              <App.Loader size={[40, 32]} />
-            ) : (
-              <App.Text size={[40, 32]} weight={700}>{results.points}</App.Text>
-            )}
-        </App.Flex>
-      </App.Flex>
-
-      <App.Flex column fullWidth gap={32} className={styles.tier}>
-        <App.Flex row align="flex-end" gap={8}>
-          <App.Text size={[24, 20]} weight={600} height={1}>Current Tier:</App.Text>
-          {loading ? (
-              <App.Loader size={[32, 24]} />
-            ) : (
-              <App.Text size={[32, 24]} weight={700} italic family="Playfair Display" color="#7364FD" height={1}>{results?.tier?.title}</App.Text>
-            )}
-        </App.Flex>
-
-        <App.Flex column fullWidth gap={8}>
-          <App.Flex align="center" justify="space-between">
-            {tournament.tiers.map(item => (
-              <App.Flex key={item.id} center className={cn(styles.tierTitle, {[styles.active]: results?.tier?.level >= item?.level})}>
-                <App.Text size={12} color={results?.tier?.level >= item?.level ? '#fff' : '#9B99AE'}>{item.title}</App.Text>
-              </App.Flex>
-            ))}
-          </App.Flex>
-
-          <App.Flex fullWidth height={22} center>
-            <div className={styles.progressBox}>
-              <div className={styles.progress} style={{ width: calculateWidth() }}>
-                <App.Flex center className={styles.progressAmount}>
-                  <App.Text size={12} color="#7364FD">{formatNumber(results.volume)}</App.Text>
-                </App.Flex>
-              </div>
-            </div>
-          </App.Flex>
-
-          <App.Flex align="center" justify="space-between">
-            {tournament.tiers.map((item, index) => (
-              <React.Fragment key={item.id}>
-                <App.Flex align="center" className={cn(styles.tierDesc)}>
-                  <App.Text center size={12} color="#9B99AE">{formatNumber(item.volume_required)} USDT</App.Text>
-                </App.Flex>
-
-                {!isMobile && index < tournament.tiers.length - 1 ? (
-                  <App.Flex flex={1} align="center" justify="space-around">
-                    <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
-                    <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
-                    <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
-                    <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
-                    <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
-                  </App.Flex>
-                ) : null}
-              </React.Fragment>
-            ))}
+                <App.Loader size={[40, 32]} />
+              ) : (
+                <App.Text size={[40, 32]} weight={700}>{results.points}</App.Text>
+              )}
           </App.Flex>
         </App.Flex>
-        
-        <App.Flex direction={['row', 'column']} align={['center', 'flex-start']} justify="space-between" gap={[0, 12]}>
+
+        <App.Flex column fullWidth gap={32} className={styles.tier}>
           <App.Flex row align="flex-end" gap={8}>
-            <App.Text size={20} weight={600} height={1}>Tier <App.Text inline size={20} weight={600} italic family="Playfair Display" height={1}>Benefits:</App.Text></App.Text>
-            <App.Text size={20} weight={700} italic family="Playfair Display" color="#7364FD" height={1}>x{results?.tier?.multiplier}</App.Text>
+            <App.Text size={[24, 20]} weight={600} height={1}>Current Tier:</App.Text>
+            {loading ? (
+                <App.Loader size={[32, 24]} />
+              ) : (
+                <App.Text size={[32, 24]} weight={700} italic family="Playfair Display" color="#7364FD" height={1}>{results?.tier?.title}</App.Text>
+              )}
           </App.Flex>
 
-          <App.Flex row center sx={{ cursor: 'pointer' }} onClick={handleHowTo}>
-            <App.Text weight={600} color="#A6DC37">How are points calculated?</App.Text>
-            <App.Icon icon="chevron-right" width={16} height={16} color="#A6DC37" />
+          <App.Flex column fullWidth gap={8}>
+            <App.Flex align="center" justify="space-between">
+              {tournament.tiers.map(item => (
+                <App.Flex key={item.id} center className={cn(styles.tierTitle, {[styles.active]: results?.tier?.level >= item?.level})}>
+                  <App.Text size={12} color={results?.tier?.level >= item?.level ? '#fff' : '#9B99AE'}>{item.title}</App.Text>
+                </App.Flex>
+              ))}
+            </App.Flex>
+
+            <App.Flex fullWidth height={22} center>
+              <div className={styles.progressBox}>
+                <div className={styles.progress} style={{ width: calculateWidth() }}>
+                  <App.Flex center className={styles.progressAmount}>
+                    <App.Text size={12} color="#7364FD">{formatNumber(results.volume)}</App.Text>
+                  </App.Flex>
+                </div>
+              </div>
+            </App.Flex>
+
+            <App.Flex align="center" justify="space-between">
+              {tournament.tiers.map((item, index) => (
+                <React.Fragment key={item.id}>
+                  <App.Flex align="center" className={cn(styles.tierDesc)}>
+                    <App.Text center size={12} color="#9B99AE">{formatNumber(item.volume_required)} USDT</App.Text>
+                  </App.Flex>
+
+                  {!isMobile && index < tournament.tiers.length - 1 ? (
+                    <App.Flex flex={1} align="center" justify="space-around">
+                      <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
+                      <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
+                      <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
+                      <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
+                      <App.Text size={10} color="#9B99AE" height={1}>&bull;</App.Text>
+                    </App.Flex>
+                  ) : null}
+                </React.Fragment>
+              ))}
+            </App.Flex>
+          </App.Flex>
+          
+          <App.Flex direction={['row', 'column']} align={['center', 'flex-start']} justify="space-between" gap={[0, 12]}>
+            <App.Flex row align="flex-end" gap={8}>
+              <App.Text size={20} weight={600} height={1}>Tier <App.Text inline size={20} weight={600} italic family="Playfair Display" height={1}>Benefits:</App.Text></App.Text>
+              <App.Text size={20} weight={700} italic family="Playfair Display" color="#7364FD" height={1}>x{results?.tier?.multiplier}</App.Text>
+            </App.Flex>
+
+            <App.Flex row center sx={{ cursor: 'pointer' }} onClick={handleHowTo}>
+              <App.Text weight={600} color="#A6DC37">How are points calculated?</App.Text>
+              <App.Icon icon="chevron-right" width={16} height={16} color="#A6DC37" />
+            </App.Flex>
           </App.Flex>
         </App.Flex>
-      </App.Flex>
 
-      <App.Flex align="center" justify="space-between" gap={16}>
-        <App.Text weight={700} color="rgba(255, 255, 255, .6)">Want to know more about the tournaments?</App.Text>
-        <a href="https://blog.tegro.com/?utm_source=homepage&utm_medium=footer&utm_campaign=testnet" target="_blank" rel="noreferrer">
-          <App.ButtonGradient icon="arrow-45">Visit Blog</App.ButtonGradient>
-        </a>
-      </App.Flex>
+        <App.Flex align="center" justify="space-between" gap={16}>
+          <App.Text weight={700} color="rgba(255, 255, 255, .6)">Want to know more about the tournaments?</App.Text>
+          <a href="https://blog.tegro.com/?utm_source=homepage&utm_medium=footer&utm_campaign=testnet" target="_blank" rel="noreferrer">
+            <App.ButtonGradient icon="arrow-45">Visit Blog</App.ButtonGradient>
+          </a>
+        </App.Flex>
 
-      <App.Dialog hideClose hideHeader width={1000} open={openModal} onClose={() => setOpenModal(false)}>
-        <HowWorks tournament={tournament} onClose={() => setOpenModal(false)} />
-      </App.Dialog>
-    </App.Flex>
-  )
+        <App.Dialog hideClose hideHeader width={1000} open={openModal} onClose={() => setOpenModal(false)}>
+          <HowWorks tournament={tournament} onClose={() => setOpenModal(false)} />
+        </App.Dialog>
+      </App.Flex>
+    ) : (
+      <App.Flex fullWidth justify="center" align="flex-start">
+        <App.ButtonGradient onClick={handleConnect}>Connect Wallet</App.ButtonGradient>
+      </App.Flex>
+    )
+  ) : null
 }
 
 export default TournamentInfo
