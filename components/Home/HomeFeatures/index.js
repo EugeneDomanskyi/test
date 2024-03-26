6a7cc2919b59ea8558ef6f31f4c58d15eb48d167
EugeneDomanskyi <eugene@tegro.com>
2024-03-26 14:05:50 +0200

    Update subtitles to H3

--- components/Home/HomeFeatures/index.js
+++ components/Home/HomeFeatures/index.js
@@ -17,7 +17,7 @@ const HomeFeatures = () => {
           <App.Flex direction={['row', 'column']} gap={24}>
             <App.Flex flex={1} className={styles.box}>
               <App.Flex fullWidth column className={cn(styles.inner, styles.inner1)}>
-                <App.Text size={[24, 20]} weight={600}>Efficient Orderbooks</App.Text>
+                <App.Text tag="h3" size={[24, 20]} weight={600}>Efficient Orderbooks</App.Text>
                 <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Trade with tighter market spreads, rivaling a CEX.</App.Text>
               </App.Flex>
             </App.Flex>
@@ -26,7 +26,7 @@ const HomeFeatures = () => {
               <App.Flex flex={[1, null]} className={styles.box}>
                 <App.Flex fullWidth className={cn(styles.inner, styles.inner2)}>
                   <App.Flex column width={[182, '100%']}>
-                    <App.Text size={[24, 20]} weight={600}>Gasless Quotes</App.Text>
+                    <App.Text tag="h3" size={[24, 20]} weight={600}>Gasless Quotes</App.Text>
                     <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Actively manage trading positions without worrying about gas.</App.Text>
                   </App.Flex>
                 </App.Flex>
@@ -35,7 +35,7 @@ const HomeFeatures = () => {
               <App.Flex flex={[1, null]} className={cn(styles.box, styles.gradient)}>
                 <App.Flex fullWidth className={cn(styles.inner, styles.inner3)}>
                   <App.Flex column width={[190, '100%']}>
-                    <App.Text size={[24, 20]} weight={600}>Lightning-fast Matching Engine</App.Text>
+                    <App.Text tag="h3" size={[24, 20]} weight={600}>Lightning-fast Matching Engine</App.Text>
                     <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Achieve peak trading performance with up to 500K trades settled in a second.</App.Text>
                   </App.Flex>
                 </App.Flex>
@@ -47,7 +47,7 @@ const HomeFeatures = () => {
             <App.Flex flex={[1, null]} className={styles.box}>
               <App.Flex fullWidth className={cn(styles.inner, styles.inner4)}>
                 <App.Flex column>
-                  <App.Text size={[24, 20]} weight={600}>Binance-like APIs</App.Text>
+                  <App.Text tag="h3" size={[24, 20]} weight={600}>Binance-like APIs</App.Text>
                   <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Import CeFi algo strategies with simple plug & play API integration.</App.Text>
                 </App.Flex>
               </App.Flex>
@@ -56,7 +56,7 @@ const HomeFeatures = () => {
             <App.Flex flex={[1, null]} className={styles.box}>
               <App.Flex fullWidth className={cn(styles.inner, styles.inner5)}>
                 <App.Flex column>
-                  <App.Text size={[24, 20]} weight={600}>Gas Efficiency</App.Text>
+                  <App.Text tag="h3" size={[24, 20]} weight={600}>Gas Efficiency</App.Text>
                   <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Save up to 3X on gas with trade roll-ups, beating other DEXs.</App.Text>
                 </App.Flex>
               </App.Flex>
@@ -65,7 +65,7 @@ const HomeFeatures = () => {
             <App.Flex flex={[1, null]} className={styles.box}>
               <App.Flex fullWidth className={cn(styles.inner, styles.inner6)}>
                 <App.Flex column>
-                  <App.Text size={[24, 20]} weight={600}>MEV Resistant</App.Text>
+                  <App.Text tag="h3" size={[24, 20]} weight={600}>MEV Resistant</App.Text>
                   <App.Text size={[16, 14]} weight={400} color="#FFFFFF99">Your trades are protected against bots or any predatory practices. Trade worry-free!</App.Text>
                 </App.Flex>
               </App.Flex>
@@ -75,7 +75,7 @@ const HomeFeatures = () => {
           <App.Flex fullWidth className={styles.box}>
             <App.Flex fullWidth className={cn(styles.inner, styles.inner7)}>
               <App.Flex column width={244}>
-                <App.Text size={24} weight={600}>Custody-less Trading</App.Text>
+                <App.Text tag="h3" size={24} weight={600}>Custody-less Trading</App.Text>
                 <App.Text size={16} weight={400} color="#FFFFFF99">Trade direct from wallet with 100% self-custody, no deposits required.</App.Text>
               </App.Flex>
             </App.Flex>
