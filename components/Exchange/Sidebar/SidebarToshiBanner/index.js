3bd887766a456d88aa7fac71b641b7efdfe9d55b
Denys Domanskyi <denys@tegro.com>
2024-05-31 15:42:23 +0300

    added toshi logo

--- components/Exchange/Sidebar/SidebarToshiBanner/index.js
+++ components/Exchange/Sidebar/SidebarToshiBanner/index.js
@@ -77,7 +77,10 @@ const SidebartoshiPopup = () => {
           
           <App.Flex center column gap={8} sx={{position: 'absolute'}}>
             <App.Text center size={24} weight={900} height={1} gradient="radial-gradient(193.17% 113.6% at 96.29% 4.49%, #FFF6A3 0%, #FFF066 34.61%, #FFCB45 68.83%, #FFBD13 100%)">2,000,000 $TOSHI<br /> in rewards!</App.Text>
-            <App.Text center size={16} weight={600} height={1} gradient="linear-gradient(180deg, #FFF 0%, #C7C7C7 100%)">TOSHI MANIA</App.Text>
+            <App.Flex gap={4}>
+              <App.Icon icon="toshi-logo" />
+              <App.Text center size={16} weight={600} height={1} sx={{marginTop: -1}} gradient="linear-gradient(180deg, #FFF 0%, #C7C7C7 100%)">MANIA</App.Text>
+            </App.Flex>
           </App.Flex>
         </App.Flex>
 
