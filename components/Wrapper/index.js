c8602809de0499a6e526e2178b5834669561f653
Denys Domanskyi <denys@tegro.com>
2024-05-31 14:27:01 +0300

    changed align of popup to stick to wrapper

--- components/Wrapper/index.js
+++ components/Wrapper/index.js
@@ -14,6 +14,7 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import StickyBanner from '@/components/StickyBanner'
 import PointsPopup from '@/components/Points/PointsPopup'
+import SidebarToshiBanner from '@/components/Exchange/Sidebar/SidebarToshiBanner'
 
 const Analytics = dynamic(import('@/components/Analytics'), {ssr: false})
 
@@ -197,6 +198,12 @@ const Wrapper = ({ children }) => {
                 {children}
                 {!isCampaign && !isApp && !isExchange && !isPD ? <Footer /> : null}
               </div>
+
+              {
+                page === 'exchange' && !isApp
+                  ? <SidebarToshiBanner />
+                  : null
+              }
             </div>
           : <Footer />
       }
