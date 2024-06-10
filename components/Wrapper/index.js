0cd9634142a950b15716a96bb0b8060935a07324
Denys Domanskyi <denys@tegro.com>
2024-06-10 22:19:32 +0300

    hide TOSHI banner

--- components/Wrapper/index.js
+++ components/Wrapper/index.js
@@ -13,7 +13,7 @@ import $point from '@/store/point'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import StickyBanner from '@/components/StickyBanner'
-import SidebarToshiBanner from '@/components/Exchange/Sidebar/SidebarToshiBanner'
+// import SidebarToshiBanner from '@/components/Exchange/Sidebar/SidebarToshiBanner'
 
 const Analytics = dynamic(import('@/components/Analytics'), {ssr: false})
 
@@ -168,12 +168,6 @@ const Wrapper = ({ children }) => {
                 {children}
                 {!isCampaign && !isApp && !isExchange && !isPD ? <Footer /> : null}
               </div>
-
-              {
-                page === 'exchange' && !isApp
-                  ? <SidebarToshiBanner />
-                  : null
-              }
             </div>
           : <Footer />
       }
