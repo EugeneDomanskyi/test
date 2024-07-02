544405702a436443c324db660bf21b522b89702a
Ievgenii Domanskyi <eugene@tegro.com>
2024-07-02 10:22:37 +0300

    Testing

--- components/Exchange/Sidebar/SidebarBanner/index.js
+++ components/Exchange/Sidebar/SidebarBanner/index.js
@@ -87,7 +87,7 @@ const SidebarBanner = () => {
           
           <App.Flex center column gap={16} sx={{position: 'absolute'}}>
             <App.Text center size={24} weight={900} height={1} gradient="radial-gradient(193.17% 113.6% at 96.29% 4.49%, #FFF6A3 0%, #FFF066 34.61%, #FFCB45 68.83%, #FFBD13 100%)">20,000,000 $MOCHI<br /> in rewards!</App.Text>
-            <App.Text center size={16} weight={700} height={1} gradient="linear-gradient(180deg, #FFF 0%, #C7C7C7 100%)">MOCHI DUSH</App.Text>
+            <App.Text center size={16} weight={700} height={1} gradient="linear-gradient(180deg, #FFF 0%, #C7C7C7 100%)">MOCHI DASH</App.Text>
           </App.Flex>
         </App.Flex>
 
