0763d459ec043a4cb51c73c1917d576b644e088e
JulianBeletskyy <julian@tegro.com>
2024-07-04 12:55:05 +0300

    streak container

---
+++ components/Gems/GemsStreak/index.js
@@ -0,0 +1,37 @@
+import styles from './styles.module.scss'
+import cn from 'classnames'
+
+const GemsStreak = ({streaks}) => {
+  console.log(streaks)
+  return (
+    <div className={styles.container} style={{height: 156}}>
+      <div className={styles.streakList}>
+        <div className={styles.line}>
+          <div className={styles.squareContainer}>
+            {
+              streaks.slice(0, streaks.length - 1).map((streak, index) => {
+                return (
+                  <div className={styles.square} />
+                )
+              })
+            }
+          </div>
+        </div>
+        {
+          streaks.map((streak, index) => {
+            return (
+              <div key={index} className={cn(styles.streakContainer)}>
+                <div className={styles.streakCircle}>
+                  <div className={styles.streakInner}>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default GemsStreak
