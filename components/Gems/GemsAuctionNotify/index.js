0dc5f31a984ebdb493e86aedf3516cf974656e97
Ievgenii Domanskyi <eugene@tegro.com>
2024-07-31 14:33:27 +0300

    Commit

--- components/Gems/GemsAuctionNotify/index.js
+++ components/Gems/GemsAuctionNotify/index.js
@@ -10,6 +10,16 @@ const GemsAuctionNotify = () => {
   const { t } = useTranslation()
   const referral = useSelector(({ $gem }) => $gem.referral)
 
+  const handleWarning = () => {
+    
+  }
+
+  const getTooltip = () => {
+    return (
+      <App.Text size={14} weight={600}>{t('Earn gems for Connecting your wallet, trading and completing quests on Tegro.')} <App.Text inline size={14} weight={600} color="#A6DC37" onClick={handleWarning}>{t('Learn More')}</App.Text></App.Text>
+    )
+  }
+
   return (
     <App.Container maxWidth={1230} sx={{ paddingBottom: 32 }}>
       <App.Flex column fullWidth flex={1} gap={16}>
@@ -17,7 +27,7 @@ const GemsAuctionNotify = () => {
           <App.Flex row align="center" order={[0, 1]} gap={24}>
             <App.Flex row center gap={16} className={styles.frame} flex={[null, 1]}>
               <App.Text size={[28, 14]} weight={600} height={1}>{t('Gems')} {referral.points ?? 0}</App.Text>
-              <App.Tooltip variant="v2" text={'Earn gems for Connecting your wallet, trading and completing quests on Tegro. Learn More'} placement="bottom">
+              <App.Tooltip variant="v2" text={getTooltip()} placement="bottom">
                 <App.Icon icon="info2" />
               </App.Tooltip>
             </App.Flex>
