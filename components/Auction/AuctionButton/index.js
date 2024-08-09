ae68e463a988d699266ce7c527b0460c304239a0
Ievgenii Domanskyi <eugene@tegro.com>
2024-08-09 14:00:29 +0300

    Testing

--- components/Auction/AuctionButton/index.js
+++ components/Auction/AuctionButton/index.js
@@ -172,6 +172,7 @@ const AuctionButton = ({ item, small, share, short, telegram }) => {
           })
 
           if (result && !result.error) {
+            dispatch($gem.set.auctionUpdated({data: {auction: result}, wallet: user.wallet}))
             dispatch($gem.set.totalGems(user.points - item.gemsPrice))
             dispatch($alert.set.success({ title: t(`Bid Placed!`), text: t(`You placed a bid for ${item.nextPrice} ${item.token.currency}.`) }))
 
