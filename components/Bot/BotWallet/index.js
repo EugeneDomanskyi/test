33dc017983bd710368fc873c0d3723f8f5cd50f3
Ievgenii Domanskyi <eugene@tegro.com>
2024-08-26 13:27:03 +0300

    Testing

--- components/Bot/BotWallet/index.js
+++ components/Bot/BotWallet/index.js
@@ -4,16 +4,8 @@ import TelegramBot from '@/libs/TelegramBot'
 const BotWallet = () => {
   const handleConnect = () => {
     TelegramBot.showPopup('Connect Wallet', 'You will be redirect to Tegro website to connect Base wallet', [{ id: 'ok', type: 'ok', text: 'Ok' }])
-    TelegramBot.on('popupClosed', (buttonId) => {
-      console.log('First', buttonId)
-      if (buttonId === 'ok') {
-        TelegramBot.openLink('https://beta.tegro.com/bot/wallet')
-      }  
-    })
-
-    TelegramBot.on('popup_closed', (buttonId) => {
-      console.log('Second', buttonId)
-      if (buttonId === 'ok') {
+    TelegramBot.on('popupClosed', (response) => {
+      if (response.button_id === 'ok') {
         TelegramBot.openLink('https://beta.tegro.com/bot/wallet')
       }  
     })
