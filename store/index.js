ed745b9013dfe6bb49c31227910366519b2497fb
Ievgenii Domanskyi <eugene@tegro.com>
2024-09-19 18:55:12 +0300

    Show errors

--- store/index.js
+++ store/index.js
@@ -98,7 +98,7 @@ export const request = async (uri, method = 'GET', {api, jwt_token, ...data} = {
 }
 
 const responseHandler = async (response) => {
-  if (response.status == 500 || response.status == 502 || response.status == 404 || response.status == 400) {
+  if (response.status == 500 || response.status == 502 || response.status == 404) {
     return null
   }
 
