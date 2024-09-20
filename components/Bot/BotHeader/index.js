a21ee1a790b6589c4f45fb8b46c85f2c68532c8b
Ievgenii Domanskyi <eugene@tegro.com>
2024-09-20 16:02:27 +0300

    Testing

--- components/Bot/BotHeader/index.js
+++ components/Bot/BotHeader/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import moment from 'moment'
 
@@ -12,14 +13,23 @@ import BotHeaderTimer from '@/components/Bot/BotHeaderTimer'
 import styles from './styles.module.scss'
 
 const BotHeader = () => {
-  const now = moment()
-
   const dispatch = useDispatch()
-  const user = useSelector(({ $bot }) => $bot.user)
   const upcomingAuction = useSelector($auction.get.upcomingAuction)
+  const earningToBeClaimedCount = useSelector($auction.get.earningToBeClaimedCount)
+
+  useEffect(() => {
+    fetchEarnings()
+  }, [])
 
   const handleTab = (uri) => () => {
     dispatch($bot.set.tab(uri))
+  }
+
+  const fetchEarnings = async () => {
+    const result = await $auction.api.earnings()
+    if (result && !result?.error) {
+      dispatch($auction.set.earnings(result))
+    }
   }
 
   return (
@@ -35,13 +45,18 @@ const BotHeader = () => {
         </App.Flex> */}
 
         <App.Flex flex={1}>
-          <App.Button variant="bot-default" small fullWidth onClick={handleTab('my-earnings')}><App.Icon icon="earn-bot" /> My Earnings</App.Button>
+          <App.Button variant="bot-default" small fullWidth onClick={handleTab('my-earnings')}>
+            <App.Icon icon="earn-bot" /> My Earnings
+            {earningToBeClaimedCount > 0 ? (
+              <App.Flex center className={styles.dot}>
+                <App.Text size={12} weight={700} height={1}>{earningToBeClaimedCount}</App.Text>
+              </App.Flex>
+            ) : null}
+          </App.Button>
         </App.Flex>
       </App.Flex>
 
       <BotHeaderTimer timestamp={upcomingAuction?.startsIn} />
-      {/* {upcomingAuction?.startsIn && ! moment(upcomingAuction?.startsIn).isBefore(now) ? (
-      ) : null} */}
     </App.Flex>
   )
 }
--- components/Bot/BotShop/index.js
+++ components/Bot/BotShop/index.js
@@ -15,9 +15,7 @@ const shopItems = [
   {
     title: 'Pile Of Gems',
     gems: 1000,
-    price: 1,
-    // REMOVE-ON-PROD
-    // price: typeof window != 'undefined' ? (TelegramBot.host() == 'tegro.com' ? 500 : 1) : 500,
+    price: typeof window != 'undefined' ? (TelegramBot.host() == 'tegro.com' ? 500 : 1) : 500,
     image: '/images/bot/shop-gems-1.png',
   },
   {
