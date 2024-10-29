d59ef06832ccca8724d891877fce9823ef904d98
Ievgenii Domanskyi <eugene@tegro.com>
2024-10-29 15:09:45 +0200

    Commit

--- components/Bot/BotShop/index.js
+++ components/Bot/BotShop/index.js
@@ -78,8 +78,52 @@ const BotShop = () => {
   }
 
   return (
-    <App.Flex column fullWidth center gap={12}>
-      <App.Flex row gap={12} justify="center" sx={{flexWrap: 'wrap'}}>
+    <App.Flex column fullWidth center gap={32} className={styles.container}>
+      <App.Flex column fullWidth gap={16}>
+        <App.Flex center className={styles.title}>
+          <Image src="/images/bot/shop-title-1.png" width={213} height={43} alt="" />
+
+          <App.Flex center className={styles.text}>
+            <App.Text size={16} weight={700} height={1} color="#FFBB01">Offers</App.Text>
+          </App.Flex>
+        </App.Flex>
+
+        <App.Flex column fullWidth gap={4} className={styles.offer}>
+          <App.Flex row align="center" justify="space-between" className={styles.header}>
+            <App.Flex row align="center" width="45%">
+              <App.Text size={16} weight={700} color="#FFBB01" sx={{ textShadow: '0px 1.484px 9.063px rgba(182, 0, 0, 0.55), 0px 1px 3px rgba(0, 0, 0, 0.25)' }}>First Purchase Discount</App.Text>
+            </App.Flex>
+
+            <App.Flex row align="center" justify="flex-end" width="45%">
+              <App.Text right size={28} weight={800} color="#FFBB01" sx={{ textShadow: '0px 1.484px 9.063px rgba(182, 0, 0, 0.55), 0px 1px 3px rgba(0, 0, 0, 0.25)' }}>50% OFF</App.Text>
+            </App.Flex>
+          </App.Flex>
+
+          <App.Flex row justify="space-between">
+            <App.Flex column align="center" width="40%" gap={4}>
+              <Image src="/images/bot/shop-image-1.png" width={76} height={76} alt="" />
+
+              <App.Text center size={13} weight={900} height={1}>10,000 gems</App.Text>
+            </App.Flex>
+
+            <App.Flex column justify="center" align="flex-start" width="40%" gap={8}>
+              <App.Text size={17} weight={700} height={1}>Pile of Gems</App.Text>
+              <App.Flex row align="center">
+                <App.Flex row align="center" gap={4}>
+                  <App.Text lineThrough size={20} weight={700} height={1}>50</App.Text>
+                  <App.Text size={20} weight={800} height={1}>25</App.Text>
+                </App.Flex>
+
+                <Image src="/images/bot/star.png" width={23} height={22} alt="" />
+              </App.Flex>
+
+              <App.Button variant="bot" small>Buy Now</App.Button>
+            </App.Flex>
+          </App.Flex>
+        </App.Flex>
+      </App.Flex>
+
+      {/* <App.Flex row gap={12} justify="center" sx={{flexWrap: 'wrap'}}>
         {
           shopItems.map((item, index) => {
             return (
@@ -101,7 +145,7 @@ const BotShop = () => {
             )
           })
         }
-      </App.Flex>
+      </App.Flex> */}
     </App.Flex>
   )
 }
