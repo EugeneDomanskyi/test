0f8f228b7628fc35c81f98eb5da3637064376158
Ievgenii Domanskyi <eugene@tegro.com>
2024-12-04 15:42:13 +0200

    Expired

--- store/auction.js
+++ store/auction.js
@@ -106,7 +106,7 @@ const auctionHistoryTemplate = (item) => {
 export const earnings_template_v2 = (item) => {
   const lastBidMoment = moment(item.last_bid_timestamp * 1000)
   const targetDate = moment('2024-12-01')
-console.log(lastBidMoment.isBefore(targetDate))
+
   return {
     id: item.id,
     name: item.product_title,
@@ -119,7 +119,7 @@ console.log(lastBidMoment.isBefore(targetDate))
     claimHash: item.claim_tx_hash,
     claimTime: moment(item.last_bid_timestamp * 1000).add(3 * 24 * 60 * 60, 'seconds'),
     ready: true,
-    expired: lastBidMoment.isBefore(targetDate),
+    expired: item.status == 7, //lastBidMoment.isBefore(targetDate),
     status: 'closed',
     readyToClaim: item.status >= 4,
   }
